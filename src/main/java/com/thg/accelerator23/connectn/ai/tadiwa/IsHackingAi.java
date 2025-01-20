44ede7e6e893df0cceed4189b4f6987f1107baa7
Tadiwanashe Dzvoti <tadiwanashe.dzvoti01@thg.com>
2025-01-20 10:22:33 +0000

    Add code

--- src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.tadiwa;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class IsHackingAi extends Player {
+  public IsHackingAi(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, IsHackingAi.class.getName());
   }
 
   @Override
