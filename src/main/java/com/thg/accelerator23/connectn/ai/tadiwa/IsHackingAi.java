90ad4e0a65d3be01d85a29ff8803802940f4f8ac
Tadiwanashe Dzvoti <tadiwanashe.dzvoti01@thg.com>
2025-01-20 15:03:09 +0000

    Added some stuff

--- src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
@@ -4,11 +4,20 @@ import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class IsHackingAi extends Player {
+  private long startTime;
+  private static final int timeLimit = 10000;
+  private Map<String, Integer> transpositionTable;
+
   public IsHackingAi(Counter counter) {
     //TODO: fill in your name here
     super(counter, IsHackingAi.class.getName());
+    this.startTime = System.nanoTime();
+    this.transpositionTable = new HashMap<>();
   }
 
   @Override
