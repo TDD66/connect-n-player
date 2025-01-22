8e32d41d46067a259c21a97720ca0b87f19ea550
Tadiwanashe Dzvoti <tadiwanashe.dzvoti01@thg.com>
2025-01-22 12:29:36 +0000

    Adjusted min depth and mx depth to be even numbers and depth only increases by 2.

--- src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/tadiwa/IsHackingAi.java
@@ -9,8 +9,8 @@ public class IsHackingAi extends Player {
   private long startTime;
   private Map<Integer, Integer> transpositionTable;
   private static final long TIME_LIMIT = 10_000_000_000_000_000L;
-  private static final int MIN_DEPTH = 1;
-  private static final int MAX_DEPTH = 4;
+  private static final int MIN_DEPTH = 2;
+  private static final int MAX_DEPTH = 6;
 
   public IsHackingAi(Counter counter) {
     //TODO: fill in your name here
@@ -36,8 +36,8 @@ public class IsHackingAi extends Player {
     int bestScore = Integer.MIN_VALUE;
     Map<Integer, Integer> spaces = populateFreeColumns(board);
 
-    for (int depth = MIN_DEPTH; depth < MAX_DEPTH; depth++) {
-      int[] result = miniMaxWithAlphaBeta(board, depth + 1, Integer.MIN_VALUE, Integer.MAX_VALUE, spaces,true);
+    for (int depth = MIN_DEPTH; depth < MAX_DEPTH; depth += 2) {
+      int[] result = miniMaxWithAlphaBeta(board, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, spaces,true);
       transpositionTable = new HashMap<>();
       if (result[1] > bestScore) {
         bestMove = result[0];
